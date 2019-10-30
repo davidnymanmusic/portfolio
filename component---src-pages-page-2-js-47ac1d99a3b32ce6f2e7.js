2a16ecc1001732e5a70d13b925ef07a683a091d0
David Nyman <David.Nyman@ibm.com>
2019-10-30 13:46:46 -0400

    Updates

---
+++ component---src-pages-page-2-js-47ac1d99a3b32ce6f2e7.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{p5nM:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),r=t.n(a),c=(t("Wbzz"),t("Bl7J")),l=t("vrFN");n.default=function(){return r.a.createElement(c.a,null,r.a.createElement(l.a,{title:"Page two"}))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-47ac1d99a3b32ce6f2e7.js.map
---
+++ styles-e50bcfec67041f7eb52c.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"8ypT":function(n,p,o){},p2bk:function(n,p,o){}}]);
+//# sourceMappingURL=styles-e50bcfec67041f7eb52c.js.map
