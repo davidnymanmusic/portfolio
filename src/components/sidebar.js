d3a24a55332528a32dabd534a55697402e0470bd
David Nyman <David.Nyman@ibm.com>
2019-10-30 13:56:54 -0400

    Deployment is configured

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -17,13 +17,22 @@ const SideBar = (props, location) => {
       name: "resume",
     },
   ]
+  const pathname =
+    "/" +
+    window.location.pathname
+      .split("/")
+      .pop()
+      .trim()
+
   return (
     // Pass on our props
     <Menu {...props}>
       {links.map(l => (
         <Link to={`${l.path}`}>
-          {window.location.pathname === l.path ? `> ${l.name}` : `/${l.name}`}
-          {window.location.pathname === l.path ? (
+          {typeof window !== "undefined" && pathname === l.path
+            ? `> ${l.name}`
+            : `/${l.name}`}
+          {typeof window !== "undefined" && pathname === l.path ? (
             <span className="blinking-cursor">|</span>
           ) : null}
         </Link>
